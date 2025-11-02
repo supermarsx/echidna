e0c70d2735fa4d48d037429c8e4578444676190c
Mariana <github_618a@imariana.com>
2025-11-02 01:21:55 +0000

    Import NotificationManager in notification controller

--- android/app/app/src/main/kotlin/com/echidna/app/system/NotificationController.kt
+++ android/app/app/src/main/kotlin/com/echidna/app/system/NotificationController.kt
@@ -2,6 +2,7 @@ package com.echidna.app.system
 
 import android.app.Notification
 import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
