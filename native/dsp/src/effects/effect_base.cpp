e00dc87fc5460737e0c599ae61ae72a2be8ef7b8
supermarsx <github_618a@imariana.com>
2025-12-05 18:52:31 +0000

    Add file-level documentation to effect_base.cpp
    Added a brief file-level comment describing the purpose of effect_base.cpp as a trivial translation unit for effect base classes, clarifying its role in the build process.

--- native/dsp/src/effects/effect_base.cpp
+++ native/dsp/src/effects/effect_base.cpp
@@ -1,3 +1,9 @@
 #include "effect_base.h"
 
+/**
+ * @file effect_base.cpp
+ * @brief Trivial translation unit for effect base classes (mostly header-only
+ * definitions). This file exists to provide a compilation unit when needed.
+ */
+
 namespace echidna::dsp::effects {}  // namespace echidna::dsp::effects
